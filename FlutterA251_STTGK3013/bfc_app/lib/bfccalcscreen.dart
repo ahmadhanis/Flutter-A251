ecb1acdb1117287714fb152e207ef735b8f31d32
Ahmad Hanis <36687333+ahmadhanis@users.noreply.github.com>
2025-10-08 10:21:52 +0800

    update bfc UI completed

--- FlutterA251_STTGK3013/bfc_app/lib/bfccalcscreen.dart
+++ FlutterA251_STTGK3013/bfc_app/lib/bfccalcscreen.dart
@@ -18,109 +18,126 @@ class _BFCCalcScreenState extends State<BFCCalcScreen> {
         backgroundColor: Colors.red,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Text('Gender'),
-                DropdownButton<String>(
-                  value: gender,
-                  items: <String>['Male', 'Female'].map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    gender = newValue!;
-                    setState(() {});
-                  },
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text('Age'),
-                SizedBox(
-                  width: 200,
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter Age',
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          margin: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(8.0),
+            color: Colors.lightGreenAccent,
+          ),
+          width: 350,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  SizedBox(width: 100, child: Text('Gender')),
+                  DropdownButton<String>(
+                    value: gender,
+                    items: <String>['Male', 'Female'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      gender = newValue!;
+                      setState(() {});
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  SizedBox(width: 100, child: Text('Age')),
+                  SizedBox(
+                    width: 200,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter Age',
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text('Weight'),
-                SizedBox(
-                  width: 200,
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter Weight',
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  SizedBox(width: 100, child: Text('Weight')),
+                  SizedBox(
+                    width: 200,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter Weight',
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text('Height'),
-                SizedBox(
-                  width: 200,
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter Height',
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  SizedBox(width: 100, child: Text('Height')),
+                  SizedBox(
+                    width: 200,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter Height',
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text('Neck'),
-                SizedBox(
-                  width: 200,
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter Neck',
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  SizedBox(width: 100, child: Text('Neck')),
+                  SizedBox(
+                    width: 200,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter Neck',
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text('Waist'),
-                SizedBox(
-                  width: 200,
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter Waist',
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  SizedBox(width: 100, child: Text('Waist')),
+                  SizedBox(
+                    width: 200,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter Waist',
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                ElevatedButton(onPressed: () {}, child: Text('Calculate BF')),
-                ElevatedButton(onPressed: () {}, child: Text('Reset')),
-              ],
-            ),
-          ],
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text('Calculate BF')),
+                  ElevatedButton(onPressed: () {}, child: Text('Reset')),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
