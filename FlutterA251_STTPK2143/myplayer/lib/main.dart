ec8f1b70c500f35d115b8d0ce94e6e213a2bc295
Ahmad Hanis <36687333+ahmadhanis@users.noreply.github.com>
2025-10-16 15:20:39 +0800

    online audio stream

--- FlutterA251_STTPK2143/myplayer/lib/main.dart
+++ FlutterA251_STTPK2143/myplayer/lib/main.dart
@@ -10,7 +10,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyPlayer());
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        appBarTheme: AppBarTheme(backgroundColor: Colors.deepOrange),
+      ),
+      home: MyPlayer(),
+    );
   }
 }
 
@@ -23,7 +30,9 @@ class MyPlayer extends StatefulWidget {
 
 class _MyPlayerState extends State<MyPlayer> {
   late AudioPlayer player = AudioPlayer();
-  
+  AudioCache audioCache = AudioCache();
+  String status = 'Not Playing';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +40,72 @@ class _MyPlayerState extends State<MyPlayer> {
       body: Center(
         child: Column(
           children: [
-            IconButton(onPressed: playMe, icon: Icon(Icons.play_arrow)),
+            Text(
+              'Playing Audio from Assets',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    iconSize: 48,
+                    onPressed: stopMe,
+                    icon: Icon(Icons.stop),
+                  ),
+                  IconButton(
+                    iconSize: 48,
+                    onPressed: playMe,
+                    icon: Icon(Icons.play_arrow),
+                  ),
+                  IconButton(
+                    iconSize: 48,
+                    onPressed: pauseMe,
+                    icon: Icon(Icons.pause),
+                  ),
+                ],
+              ),
+            ),
+            Text(status),
+            const SizedBox(height: 20),
+            Text(
+              'Playing from online URL',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    iconSize: 48,
+                    onPressed: stopMe,
+                    icon: Icon(Icons.stop),
+                  ),
+                  IconButton(
+                    iconSize: 48,
+                    onPressed: playMeURL,
+                    icon: Icon(Icons.play_arrow),
+                  ),
+                  IconButton(
+                    iconSize: 48,
+                    onPressed: pauseMe,
+                    icon: Icon(Icons.pause),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -39,6 +113,30 @@ class _MyPlayerState extends State<MyPlayer> {
   }
 
   void playMe() {
-    player.play(AssetSource('bell.mp3'));
+    player.play(AssetSource('sonne.mp3'));
+    setState(() {
+      status = 'Playing';
+    });
+  }
+
+  void stopMe() {
+    player.stop();
+    setState(() {
+      status = 'Stop';
+    });
+  }
+
+  void pauseMe() {
+    player.pause();
+    setState(() {
+      status = 'Pause';
+    });
+  }
+
+  void playMeURL() {
+    player.play(UrlSource('https://slumberjer.com/mysong/sonne.mp3'));
+    setState(() {
+      status = 'Playing from URL';
+    });
   }
 }
