0f44eef618d3ad558d59c9472f490fedc458f0f8
Ahmad Hanis <36687333+ahmadhanis@users.noreply.github.com>
2025-11-26 10:15:47 +0800

    Add service provider contact details and actions
    Implemented a dialog in mainpage.dart to display service provider details, including contact options (call, SMS, email, WhatsApp) using url_launcher. Added getuserdetails.php API endpoint to fetch user details. Updated dependencies to include intl and url_launcher, and registered url_launcher plugins for all platforms. Also updated the baseUrl in myconfig.dart and improved AndroidManifest queries for SMS, call, and custom tabs support.

--- FlutterA251_STTGK3013/myfuwu/lib/views/mainpage.dart
+++ FlutterA251_STTGK3013/myfuwu/lib/views/mainpage.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'package:myfuwu/models/myservice.dart';
 import 'package:myfuwu/myconfig.dart';
 import 'package:myfuwu/views/loginpage.dart';
 import 'package:myfuwu/models/user.dart';
 import 'package:myfuwu/shared/mydrawer.dart';
-import 'package:myfuwu/views/myservicepage.dart';
 import 'package:myfuwu/views/newservicepage.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainPage extends StatefulWidget {
   final User? user;
@@ -21,6 +22,7 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   List<MyService> listServices = [];
   String status = "Loading...";
+  DateFormat formatter = DateFormat('dd/MM/yyyy hh:mm a');
   @override
   void initState() {
     // TODO: implement initState
@@ -93,7 +95,12 @@ class _MainPageState extends State<MainPage> {
                               ],
                             ),
                             trailing: IconButton(
-                              onPressed: () {},
+                              onPressed: () async {
+                                User owner;
+                                //get service owner details before show details
+                                owner = await getServiceOwnerDetails(index);
+                                showDetailsDialog(index, owner);
+                              },
                               icon: Icon(Icons.arrow_forward_ios),
                             ),
                           ),
@@ -136,30 +143,34 @@ class _MainPageState extends State<MainPage> {
 
   void loadServices(String searchQuery) {
     // TODO: implement loadServices
-    http.get(Uri.parse('${MyConfig.baseUrl}/myfuwu/api/loadservices.php?search=$searchQuery')).then(
-      (response) {
-        if (response.statusCode == 200) {
-          var jsonResponse = response.body;
-          var data = jsonDecode(jsonResponse);
-          listServices.clear();
-          for (var item in data['data']) {
-            listServices.add(MyService.fromJson(item));
+    http
+        .get(
+          Uri.parse(
+            '${MyConfig.baseUrl}/myfuwu/api/loadservices.php?search=$searchQuery',
+          ),
+        )
+        .then((response) {
+          if (response.statusCode == 200) {
+            var jsonResponse = response.body;
+            var data = jsonDecode(jsonResponse);
+            listServices.clear();
+            for (var item in data['data']) {
+              listServices.add(MyService.fromJson(item));
+            }
+            setState(() {
+              status = "";
+            });
+            // print(jsonResponse);
+          } else {
+            setState(() {
+              status = "Failed to load services";
+            });
           }
-          setState(() {
-            status = "";
-          });
-          // print(jsonResponse);
-        } else {
-          setState(() {
-            status = "Failed to load services";
-          });
-        }
-      },
-    );
+        });
   }
 
   void showSearchDialog() {
-    TextEditingController searchController = TextEditingController(); 
+    TextEditingController searchController = TextEditingController();
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -167,9 +178,7 @@ class _MainPageState extends State<MainPage> {
           title: Text('Search'),
           content: TextField(
             controller: searchController,
-            decoration: InputDecoration(
-              hintText: 'Enter search query',
-            ),
+            decoration: InputDecoration(hintText: 'Enter search query'),
           ),
           actions: [
             TextButton(
@@ -194,5 +203,275 @@ class _MainPageState extends State<MainPage> {
         );
       },
     );
+  }
+
+  void showDetailsDialog(int index, User owner) {
+    String formattedDate = formatter.format(
+      DateTime.parse(listServices[index].serviceDate.toString()),
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(listServices[index].serviceTitle.toString()),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.network(
+                  '${MyConfig.baseUrl}/myfuwu/assets/services/service_${listServices[index].serviceId}.PNG',
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 10),
+                Table(
+                  border: TableBorder.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                    style: BorderStyle.solid,
+                  ),
+                  columnWidths: {
+                    0: FixedColumnWidth(100.0),
+                    1: FlexColumnWidth(),
+                  },
+                  children: [
+                    TableRow(
+                      children: [
+                        TableCell(
+                          // Use TableCell to apply consistent styling/padding
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Title'),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              listServices[index].serviceTitle.toString(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Description'),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              listServices[index].serviceDesc.toString(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Type'),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              listServices[index].serviceType.toString(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('District'),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              listServices[index].serviceDistrict.toString(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Rate'),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              listServices[index].serviceRate.toString(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Date'),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(formattedDate),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Provider'),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(owner.userName.toString()),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Phone'),
+                          ),
+                        ),
+                        TableCell(
+                          verticalAlignment: TableCellVerticalAlignment.middle,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(owner.userPhone.toString()),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () async {
+                        await launchUrl(
+                          Uri.parse('tel:${owner.userPhone.toString()}'),
+                          mode: LaunchMode.externalApplication,
+                        );
+                      },
+                      icon: Icon(Icons.call),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        await launchUrl(
+                          Uri.parse(
+                            'sms:${owner.userPhone.toString()}',
+                          ),
+                          mode: LaunchMode.externalApplication,
+                        );
+                      },
+                      icon: Icon(Icons.message),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        await launchUrl(
+                          Uri.parse('mailto:${owner.userEmail.toString()}'),
+                          mode: LaunchMode.externalApplication,
+                        );
+                      },
+                      icon: Icon(Icons.email),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        await launchUrl(
+                          Uri.parse(
+                            'https://wa.me/${owner.userPhone.toString()}',
+                          ),
+                          mode: LaunchMode.externalApplication,
+                        );
+                      },
+                      icon: Icon(Icons.wechat),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<User> getServiceOwnerDetails(int index) async {
+    String ownerid = listServices[index].userId.toString();
+    User owner = User();
+    try {
+      final response = await http.get(
+        Uri.parse(
+          '${MyConfig.baseUrl}/myfuwu/api/getuserdetails.php?userid=$ownerid',
+        ),
+      );
+      if (response.statusCode == 200) {
+        var jsonResponse = response.body;
+        var resarray = jsonDecode(jsonResponse);
+        if (resarray['status'] == 'success') {
+          owner = User.fromJson(resarray['data'][0]);
+        }
+      }
+    } catch (e) {
+      print('Error fetching user details: $e');
+    }
+    return owner;
   }
 }
